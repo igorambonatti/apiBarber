3acdc1f31ffa53026427fa2a3e431366d4a1cf9f
Igor ambonatti <contatoambonatti@gmail.com>
2020-01-07 18:38:50 -0300

    upload

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '709fb0691fc3b9',
+    pass: '305fda72efd168',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://62d8ec74cec7407db09ecf414958303f@sentry.io/1874192',
+};
